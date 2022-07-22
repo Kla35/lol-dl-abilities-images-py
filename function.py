07c3d2e83d3e0500cfb3ed0374da74f16ddc6364
Kla35 <nathan35.minecraft@gmail.com>
2022-07-22 19:41:10 +0200

    v.1.0.1 - Move functions from main.py to function.py

--- function.py
+++ function.py
@@ -1,5 +1,9 @@
+import os
+import constant
+import shutil
 import urllib.request
 import json
+import constant
 
 ######### DOWNLOAD DATA FUNCTION ##########
 
@@ -72,9 +76,61 @@ def defineSpellKey(str):
         case _:
             return str
 
+######## DOWNLOAD IMAGES FUNCTION ########
+def download_image(url, file_path, file_name):
+    full_path = file_path + '/' + file_name
+    req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    try:
+        response = urllib.request.urlopen( req )
+    except urllib.error.URLError as e:
+        if hasattr( e, 'reason' ):
+            print( 'Fail in reaching the server -> ', e.reason )
+            print(url)
+            return False
+        elif hasattr( e, 'code' ):
+            print( 'The server couldn\'t fulfill the request -> ', e.code )
+            print(url)
+            return False
+    else:
+        with open( full_path, 'wb' ) as fo:
+            fo.write( response.read() )
+            #print( 'Url saved as %s' % full_path )
+        return True
+
+def downloadSpellImage(spellPath, fileName):
+    imageURL = ""
+    if spellPath.startswith('/lol-game-data/assets/ASSETS/'):
+        trim = spellPath.split('/lol-game-data/assets/ASSETS/')
+        imageURL = 'https://raw.communitydragon.org/latest/game/assets/'+trim[1].lower()
+    else:
+        trim = spellPath.split('/lol-game-data/assets/v1/champion-ability-icons/')
+        imageURL = 'https://raw.communitydragon.org/latest/game/data/characters/qiyana/hud/icons2d/'+trim[1].lower()
+    
+    download_image(imageURL, constant.SPELL_FOLDER, fileName)
+
+def downloadIconImage(champName,version):
+    imageURL = 'http://ddragon.leagueoflegends.com/cdn/'+version+'/img/champion/'+champName+'.png';
+    download_image(imageURL, constant.ICON_FOLDER, champName+'.jpg')
+
+def downloadSplashImage(champName):
+    imageURL = 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/'+champName+'_0.jpg';
+    download_image(imageURL, constant.SPLASH_FOLDER, champName+'.jpg')
+
+def downloadVerticalImage(champName):
+    imageURL = 'https://ddragon.leagueoflegends.com/cdn/img/champion/loading/'+champName+'_0.jpg';
+    download_image(imageURL, constant.VERTICAL_FOLDER, champName+'.jpg')
+
 ######## GENERAL FUNCTION ###########
 
 def getJSONData(url):
     json_url = urllib.request.urlopen(url)
     data = json.loads(json_url.read())
     return data
+
+
+    
+def recreate_folders():
+    folders = [constant.ICON_FOLDER, constant.SPELL_FOLDER,constant.SPLASH_FOLDER,constant.VERTICAL_FOLDER]
+    for folder in folders:
+        shutil.rmtree(folder, ignore_errors=True)
+        os.makedirs(folder)
